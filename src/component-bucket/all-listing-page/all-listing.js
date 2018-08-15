25b9ef57911453e6b437792b29da26eadeda9358
sricharankrishnan <sricharan@choosemybicycle.com>
2018-08-15 18:48:28 +0530

    Link Updates placed in All Listing Page

--- src/component-bucket/all-listing-page/all-listing.js
+++ src/component-bucket/all-listing-page/all-listing.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Row, Col, Button, Clearfix } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./all-listing.css";
 
 import axios from "axios";
@@ -9,68 +10,80 @@ const WrapperObject = (props) => {
   return props.children;
 };
 
-const MovieListing = ({imageUrl, movieName, rating, releaseDate}) => {
+const MovieListing = ({imageUrl, movieName, rating, releaseDate, id}) => {
+  const queryUrl = "/movie-bio?qt=" + movieName.toLowerCase().split(" ").join("___") + "&st=movie" + "&qid=" + id;
+  console.log(queryUrl);
   return (
     <Col xs={12} sm={4} lg={3} className="movieListing">
-      <div className="borderBoxContainer">
-        <div className="imageContainer positionRelative">
-          <img src={imageUrl} className="img-responsive center-block" alt={movieName} title={movieName}/>
-          <div className="nameContainer">
-            <p>{movieName}</p>
+      <Link to={queryUrl}>
+        <div className="borderBoxContainer">
+          <div className="imageContainer positionRelative">
+            <img src={imageUrl} className="img-responsive center-block" alt={movieName} title={movieName}/>
+            <div className="nameContainer">
+              <p>{movieName}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="footerContainer">
-        <ul className="list-unstyled metaList row">
-          <li className="releaseDate"><b>RELEASE DATE:</b> {releaseDate}</li>
-          <li className="viewDetails">
-            <img src="./assets/icons/expand-icon.svg" className="img-responsive center-block" alt="View Details" title="View Details"/>
-          </li>
-        </ul>
-      </div>
+        <div className="footerContainer">
+          <ul className="list-unstyled metaList row">
+            <li className="releaseDate"><b>RELEASE DATE:</b> {releaseDate}</li>
+            <li className="viewDetails">
+              <img src="./assets/icons/expand-icon.svg" className="img-responsive center-block" alt="View Details" title="View Details"/>
+            </li>
+          </ul>
+        </div>
+      </Link>
     </Col>
   );
 };
-const TvOnAir = ({imageUrl, movieName, rating, releaseDate}) => {
+
+const TvOnAir = ({imageUrl, movieName, rating, releaseDate, id}) => {
+  const queryUrl = "/tv-bio?qt=" + movieName.toLowerCase().split(" ").join("___") + "&st=tv" + "&qid=" + id;
   return (
     <Col xs={12} sm={4} lg={3} className="movieListing">
-      <div className="borderBoxContainer">
-        <div className="imageContainer positionRelative">
-          <img src={imageUrl} className="img-responsive center-block" alt={movieName} title={movieName}/>
-          <div className="nameContainer">
-            <p>{movieName}</p>
+      <Link to={queryUrl}>
+        <div className="borderBoxContainer">
+          <div className="imageContainer positionRelative">
+            <img src={imageUrl} className="img-responsive center-block" alt={movieName} title={movieName}/>
+            <div className="nameContainer">
+              <p>{movieName}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="footerContainer">
-        <ul className="list-unstyled metaList row">
-          <li className="releaseDate"><b>FIRST AIRED ON:</b> {releaseDate}</li>
-          <li className="viewDetails">
-            <img src="./assets/icons/expand-icon.svg" className="img-responsive center-block" alt="View Details" title="View Details"/>
-          </li>
-        </ul>
-      </div>
+        <div className="footerContainer">
+          <ul className="list-unstyled metaList row">
+            <li className="releaseDate"><b>FIRST AIRED ON:</b> {releaseDate}</li>
+            <li className="viewDetails">
+              <img src="./assets/icons/expand-icon.svg" className="img-responsive center-block" alt="View Details" title="View Details"/>
+            </li>
+          </ul>
+        </div>
+      </Link>
     </Col>
   );
 };
-const PopularPeople = ({imageUrl, personName}) => {
+
+const PopularPeople = ({imageUrl, personName, id}) => {
+  const queryUrl = "/person-bio?qt=" + personName.toLowerCase().split(" ").join("___") + "&st=person" + "&qid=" + id;
   return (
     <Col xs={12} sm={4} lg={3} className="movieListing">
-      <div className="borderBoxContainer">
-        <div className="imageContainer positionRelative">
-          <img src={imageUrl} className="img-responsive center-block" alt={personName} title={personName}/>
-          <div className="nameContainer">
-            <p>{personName}</p>
+      <Link to={queryUrl}>
+        <div className="borderBoxContainer">
+          <div className="imageContainer positionRelative">
+            <img src={imageUrl} className="img-responsive center-block" alt={personName} title={personName}/>
+            <div className="nameContainer">
+              <p>{personName}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="footerContainer">
-        <ul className="list-unstyled metaList row">
-          <li className="viewDetails">
-            <img src="./assets/icons/expand-icon.svg" className="img-responsive center-block" alt="View Details" title="View Details"/>
-          </li>
-        </ul>
-      </div>
+        <div className="footerContainer">
+          <ul className="list-unstyled metaList row">
+            <li className="viewDetails">
+              <img src="./assets/icons/expand-icon.svg" className="img-responsive center-block" alt="View Details" title="View Details"/>
+            </li>
+          </ul>
+        </div>
+      </Link>
     </Col>
   );
 };
@@ -164,7 +177,7 @@ class AllListing extends Component {
             tvTempArray = [],
             peopleTempArray = [];
 
-        moviesApiResponse.forEach(({poster_path, original_title, vote_average, release_date}, thisIndex) => {
+        moviesApiResponse.forEach(({poster_path, original_title, vote_average, release_date, id}, thisIndex) => {
           if(thisIndex <= 11) {
             let d = new Date(String(release_date)),
                 movieRelease = d.getDate() + " " + monthArray[d.getMonth()] + " " + d.getFullYear();
@@ -172,11 +185,13 @@ class AllListing extends Component {
               imageUrl: "https://image.tmdb.org/t/p/original" + poster_path,
               movieName: original_title,
               rating: vote_average,
-              releaseDate: movieRelease
+              releaseDate: movieRelease,
+              id
             });
           }
         });
-        tvApiResponse.forEach(({poster_path, original_name, vote_average, first_air_date}, thisIndex) => {
+        tvApiResponse.forEach(({poster_path, original_name, vote_average, first_air_date, id}, thisIndex) => {
+          console.log(id);
           if(thisIndex <= 11) {
             let d = new Date(String(first_air_date)),
                 movieRelease = d.getDate() + " " + monthArray[d.getMonth()] + " " + d.getFullYear();
@@ -184,15 +199,17 @@ class AllListing extends Component {
               imageUrl: "https://image.tmdb.org/t/p/original" + poster_path,
               movieName: original_name,
               rating: vote_average,
-              releaseDate: movieRelease
+              releaseDate: movieRelease,
+              id
             });
           }
         });
-        peopleApiResponse.forEach(({profile_path, name}, thisIndex) => {
+        peopleApiResponse.forEach(({profile_path, name, id}, thisIndex) => {
           if(thisIndex <= 11) {
             peopleTempArray.push({
               imageUrl: "https://image.tmdb.org/t/p/original" + profile_path,
               personName: name,
+              id
             });
           }
         });
@@ -221,6 +238,7 @@ class AllListing extends Component {
             {
               !!this.state.moviesNowPlaying &&
                 this.state.moviesNowPlaying.map((thisMovieObject, thisIndex) => {
+                  console.log(thisMovieObject);
                   if((thisIndex + 1) % 4 === 0) {
                     return (
                       <WrapperObject key={thisMovieObject.movieName}>
@@ -318,6 +336,7 @@ class AllListing extends Component {
 
   componentDidMount() {
     if(!this.state.moviesNowPlaying && !this.state.tvShowsNowPlaying) {
+      console.log("what");
       this.initiatePageLoadApis();
     }
   };
